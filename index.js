4ffc7800577376ce265d26aecd6cb7db1fe7b645
sagardey23 <sagarinthesky@gmail.com>
2022-03-24 12:51:01 +0530

    first

---
+++ index.js
@@ -0,0 +1,130 @@
+
+
+
+// using if else
+// var age=25;
+
+// // if(age<=30){
+// // console.log("Adult")
+// // }
+// // else{
+// // console.log("old")
+// // }
+
+
+// using ternary operator
+
+// var age=15;
+
+// var vote= (age<18 ? "you are not eligible for vote" : "You are eligible for vote")
+
+// console.log(vote)
+
+// var age=5;
+// console.log(  age<30   ? "Adult"  :  "old"  );
+
+
+ // Take a string variable age 
+        //          a) if age less than 30 , print ""Adult"" , else ""Old"", use if else and ternary operator
+
+
+// var age=24;
+
+// if (age<30){
+//     console.log("Adult")
+// }
+// else{
+//     console.log("Old")
+// }
+        //          b) if age less than 15 , print ""Young"" , age greater than 15 but less than 30 print ""Adult"" else ""Old"" by using if else ladder
+
+
+// var age= 14;
+// if(age<15){
+//     console.log("Young");
+// }
+// else if(age>15 && age<30){
+//     console.log("Adult");
+// }
+// else{
+//     console.log("old")
+// }
+
+
+
+        // Q 5 : Use switch case and create Week calendar Monday - print 0 , Tuesday - print 1 and so on Sunday - print 6
+
+
+var num=3;
+
+switch(num){
+    case 0 : {
+        console.log("Monday");
+        break;
+    }
+    case 1 : {
+        console.log("Tuesday");
+        break;
+    }        
+    case 2 : {
+        console.log("Wednesday");
+        break;
+    }  
+    case 3 : {
+        console.log("Thrusday");
+        break;
+    }  
+    case 4 : {
+        console.log("FRiday");
+        break;
+    }
+    case 5 : {
+        console.log("Saturday");
+        break;
+    }
+    case 6 : {
+        console.log("Sunday");
+        break;
+    }
+    default : {
+        console.log("Invaild Choice");
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
